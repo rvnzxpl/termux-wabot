ec5ff5fb7dd26b4ae75f65670fa46336f0bf5bd0
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-02 12:05:13 +0700

    Create help.js

---
+++ help.js
@@ -0,0 +1,12 @@
+const help = (pushname, prefix, botName, ownerName) => {
+        return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.1
+   ╿
+┯┷ *〈 MAKER 〉*
+╽
