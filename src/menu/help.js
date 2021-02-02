c7a43d3997410081ac9c051e40fc903b22a88872
Rvnxpl12 <78360789+rvnzxpl@users.noreply.github.com>
2021-02-02 14:26:03 +0700

    Update help.js

--- src/menu/help.js
+++ src/menu/help.js
@@ -8,7 +8,33 @@ const help = (pushname, prefix, botName, ownerName) => {
    ┠≽ *Creator* : ${ownerName}
    ┠≽ *Version* : 0.0.1
    ╿
-┯┷ *〈 MAKER 〉*
-╽"
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ ${prefix}info
+┠≽ ${prefix}blocklist
+╿
+┷┯ *〈 MAKER 〉*
+   ╽
+   ┠≽ ${prefix}sticker
+   ┠≽ ${prefix}toimg
+   ╿
+┯┷ *〈 GROUP 〉*
+╽
+┠≽ ${prefix}tagall
+┠≽ ${prefix}tagall2
+┠≽ ${prefix}tagall3
+┠≽ ${prefix}add 628885xxxxxx
+┠≽ ${prefix}kick @tag
+┠≽ ${prefix}promote @tag
+┠≽ ${prefix}demote @tag
+┠≽ ${prefix}listadmins
+┠≽ ${prefix}simih 1/0
+┠≽ ${prefix}welcome 1/0
+╿
+┷┯ *〈 OTHER 〉*
+   ╽
+   ┠≽ ${prefix}wait
+   ╿ *${ownerName},*
+   ╰╼≽ *Developer © ${botName}*`
 }
 exports.help = help
